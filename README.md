5a9349b3ce323ad6021970e554d0710e80f0831d
AndreArrepia <andrearrepiaa@gmail.com>
2017-08-28 16:10:20 +0100

    First commit

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ To run the project:
 1- Install node.js @ https://nodejs.org/en/
 2- npm install -g bower //install bower
 3- npm install -g polymer-cli // install polymer-cli
-4- bower install Polymer/polymer#^2.0.0
+4- bower install
 5- polymer serve
+7- change books-api to xampp server 
---
+++ books-api/index.php
@@ -0,0 +1,40 @@
+<?php
+
+require(__DIR__ . "/src/GoodRead.php");
+
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+$cache_dir = __DIR__  . "/cache/";
+$query = (is_string($_GET["query"])) ? filter_input(INPUT_GET, "query") : "";
+if (empty($query)) {
+    $result = array(
+        "Request" => array(
+            'authentication' => 'false',
+            'key' => array(),
+            'method' => array()
+        ),
+        "search" => array(
+            'query' => array (),
+            'results-start' => '1',
+            'results-end' => '0',
+            'total-results' => '0',
+            'source' => 'Goodreads',
+            'query-time-seconds' => '0.00',
+            'results' => array()
+        )
+    );
+} else {
+
+    if (!is_dir($cache_dir)) {
+        mkdir($cache_dir, 0755);
+    }
+
+    $page = (isset($_GET["page"])) ? filter_input(INPUT_GET, "page") : 1;
+    $field = (isset($_GET["field"])) ? filter_input(INPUT_GET, "field") : "all";
+
+    $goodRead = new GoodRead("bZFY4Rc5TZpBEc89fv7XKA", $cache_dir);
+    $result = $goodRead->search($query, $page, $field);
+}
+
+ echo json_encode($result);
